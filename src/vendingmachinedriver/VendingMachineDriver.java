b76cecf40901b6b0c56b03d940377066f41afea0
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-20 19:47:04 -0700

    Added user input and output

--- src/vendingmachinedriver/VendingMachineDriver.java
+++ src/vendingmachinedriver/VendingMachineDriver.java
@@ -6,10 +6,39 @@
  */
 package vendingmachinedriver;
 
+import java.util.Scanner;
+
 public class VendingMachineDriver {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        int quantity; //stores the quantity added
+        Scanner stdIn = new Scanner(System.in);
+        VendingMachine mountainDew = new VendingMachine("Mountain Dew", 1.50, 25);
+        VendingMachine water = new VendingMachine("water", 1.00, 15);
+        VendingMachine appleJuice = new VendingMachine("Apple juice", 2.00, 20);
+        
+        System.out.println("Module 3 Competency by Hunter Quantz\n");
+        System.out.println("Starting inventory:");
+        
+        mountainDew.displayItem();
+        water.displayItem();
+        appleJuice.displayItem();
+        
+        System.out.print("How many bottles of Mountain Dew are you delivering? ");
+        quantity = stdIn.nextInt();
+        mountainDew.addStock(quantity);
+        System.out.print("How many bottles of water are you delivering? ");
+        quantity = stdIn.nextInt();
+        water.addStock(quantity);
+        System.out.print("How many bottles of apple juice are you delivering? ");
+        quantity = stdIn.nextInt();
+        appleJuice.addStock(quantity);
+        
+        System.out.println("Current Inventory:");
+        mountainDew.displayItem();
+        water.displayItem();
+        appleJuice.displayItem();
+        
     }//end main
     
 }//end VendingMachineDriver Class
