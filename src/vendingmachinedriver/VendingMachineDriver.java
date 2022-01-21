bf87e8bf77006cde47d381863028edfe92c436e2
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-20 18:55:53 -0700

    Created initial files

---
+++ src/vendingmachinedriver/VendingMachineDriver.java
@@ -0,0 +1,15 @@
+/*
+ * VendingMachineDriver.java
+ * Hunter Quantz
+ * 
+ * This program is the driver for the VendingMachine.java class
+ */
+package vendingmachinedriver;
+
+public class VendingMachineDriver {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }//end main
+    
+}//end VendingMachineDriver Class
