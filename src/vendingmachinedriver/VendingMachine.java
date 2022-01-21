b9b9a06f12583233764b823c46eb323a2aed9321
hquantz <hunter.quantz9302@mail.davistech.edu>
2022-01-20 19:25:17 -0700

    Created the VendingMachine class

---
+++ src/vendingmachinedriver/VendingMachine.java
@@ -0,0 +1,29 @@
+/*
+ * VendingMachine.java
+ * Hunter Quantz
+ * 
+ * This program has the variables and methods for the vending machines
+ */
+package vendingmachinedriver;
+
+public class VendingMachine {
+    private String name;//hold the name of the item
+    private double price;//hold the price of the item
+    private int quantity;//hold the quantity of the item
+    
+    public VendingMachine(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    
+    public void displayItem(){
+        System.out.printf("Name: %s, price: $%.2f, Quantity: %d");
+    }
+    
+    public void addStock(int quantity){
+        double value = quantity * this.price;//stores the value of the added goods
+        
+        System.out.printf("Recieved $%f of %s", value, this.name);
+    }
+}//end VendingMachine class
